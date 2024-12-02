0403ca4890fa58df1441493c001e16d86b1d4fdd
fabriciotamai <59937379+fabriciotamai@users.noreply.github.com>
2024-12-02 10:02:24 -0300

    (feat)usecases:changepassword

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,16 +1,21 @@
 import { adminRegisterUsersHandler } from '@/http/controllers/admin/admin-register-users-handler';
 import { authMiddleware } from '@/middlewares/auth/auth-middleware';
 import { FastifyInstance } from 'fastify';
+import { changePasswordHandler } from './controllers/users/change-password-handler';
 import { userSiginHandler } from './controllers/users/user-sigin-handler';
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
-  
   app.register(async (publicRoutes) => {
     publicRoutes.post('/admin/register', adminRegisterUsersHandler);
     publicRoutes.post('/users/signin', userSiginHandler);
   });
 
-  app.register(async (protectedRoutes) => {
-    protectedRoutes.addHook('onRequest', authMiddleware);
-  });
+
+  app.register(
+    async (protectedRoutes) => {
+      protectedRoutes.addHook('preHandler', authMiddleware); 
+      protectedRoutes.post('/users/change-password', changePasswordHandler);
+    },
+  
+  );
 }
--- src/middlewares/auth/auth-middleware.ts
+++ src/middlewares/auth/auth-middleware.ts
@@ -6,35 +6,44 @@ const userRepository = new PrismaUsersRepository();
 const authService = new AuthService(userRepository);
 
 export async function authMiddleware(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+ 
+
   try {
     const authHeader = request.headers.authorization;
+   
 
-    if (!authHeader) {
-      return reply.status(401).send({ error: 'Token ausente.' });
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+      console.error('Erro: Token ausente ou mal formatado.');
+      return reply.status(401).send({ error: 'Token ausente ou inválido.' });
     }
 
     const token = authHeader.split(' ')[1];
+   
+
     const payload = authService.validateToken(token);
+   
 
     const user = await authService.getUserById(payload.id);
     if (!user) {
+    
       return reply.status(401).send({ error: 'Usuário não encontrado.' });
     }
 
     if (!authService.isTokenVersionValid(payload.tokenVersion, user.tokenVersion)) {
+      
       return reply.status(401).send({ error: 'Token inválido ou expirado.' });
     }
 
-    if (user.mustChangePassword) {
-      request.user = { ...payload, mustChangePassword: true };
-      return reply.status(403).send({
-        error: 'É necessário redefinir a senha.',
-        mustChangePassword: true,
-      });
-    }
+    request.user = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+      tokenVersion: user.tokenVersion,
+    };
 
-    request.user = payload;
+   
   } catch (error) {
+  
     return reply.status(401).send({ error: 'Falha na autenticação.' });
   }
 }

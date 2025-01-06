7291a5f5a5c831c982260ff54c99e19e55a733ef
fabriciotamai <59937379+fabriciotamai@users.noreply.github.com>
2025-01-06 14:00:32 -0300

    fix(factorie)user-deposit

--- src/use-cases/factories/users/make-user-deposit-use-case.ts
+++ src/use-cases/factories/users/make-user-deposit-use-case.ts
@@ -1,7 +1,10 @@
 import { PrismaDepositsRepository } from '@/repositories/prisma/prisma-deposit-repository';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { CreateDepositUseCase } from '@/use-cases/users/user-create-deposit-use-case';
 
 export function makeCreateDepositUseCase(): CreateDepositUseCase {
   const depositsRepository = new PrismaDepositsRepository();
-  return new CreateDepositUseCase(depositsRepository);
+  const usersRepository = new PrismaUsersRepository();
+
+  return new CreateDepositUseCase(depositsRepository, usersRepository);
 }

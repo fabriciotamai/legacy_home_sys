80816e4b77437b157ced3b3e1aa0d39613233d14
fabriciotamai <59937379+fabriciotamai@users.noreply.github.com>
2025-01-07 07:07:04 -0300

    feat(usecase)user-add-address

---
+++ src/use-cases/users/user-add-address-use-case.ts
@@ -0,0 +1,40 @@
+import { AddressRepository } from '@/repositories/address-repository';
+
+interface AddAddressInput {
+  userId: number;
+  street: string;
+  number: string;
+  complement?: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  country: string;
+}
+
+export class AdminAddAddressUseCase {
+  constructor(private readonly addressRepository: AddressRepository) {}
+
+  async execute(input: AddAddressInput): Promise<void> {
+    const { userId, street, number, complement, neighborhood, city, state, postalCode, country } = input;
+
+    const existingAddresses = await this.addressRepository.findByUserId(userId);
+    if (existingAddresses.length > 0) {
+      throw new Error('Usuário já possui um endereço cadastrado.');
+    }
+
+    await this.addressRepository.create({
+      user: { connect: { id: userId } },
+      street,
+      number,
+      complement,
+      neighborhood,
+      city,
+      state,
+      postalCode,
+      country,
+    });
+
+    await this.addressRepository.updateUserComplianceStatus(userId, 'PENDING_DOCUMENTS');
+  }
+}
